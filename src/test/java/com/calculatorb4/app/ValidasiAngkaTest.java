eb1616856343c6d31d36bac2a2259a089fcc365b
Revandi2245 <revandi.faudiamar.tif422@polban.ac.id>
2025-05-04 22:45:51 +0700

    nambah index

--- src/test/java/com/calculatorb4/app/ValidasiAngkaTest.java
+++ src/test/java/com/calculatorb4/app/ValidasiAngkaTest.java
@@ -37,7 +37,7 @@ public class ValidasiAngkaTest{
         // 2. Memanggil fungsi hitung
         boolean hasil = KalkulatorSederhana.validasiAngka(angka);
         // 3. Mengecek jika hasilnya adalah null
-        assertTrue(hasil);
+        assertFalse(hasil);
     }
 
 }
